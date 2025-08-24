28f6a1c4e673a923b6c15e19e26c19b942171376
AlyTarek22 <alitarekali03@gmail.com>
2025-08-25 00:44:02 +0300

    Initial commit: Sudoku project

---
+++ build/CMakeFiles/SudokuProject.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Aly/Software Course/Projects/Sudoku Project/src/SudokuBoard.cpp" "CMakeFiles/SudokuProject.dir/src/SudokuBoard.cpp.obj" "gcc" "CMakeFiles/SudokuProject.dir/src/SudokuBoard.cpp.obj.d"
+  "D:/Aly/Software Course/Projects/Sudoku Project/src/SudokuGame.cpp" "CMakeFiles/SudokuProject.dir/src/SudokuGame.cpp.obj" "gcc" "CMakeFiles/SudokuProject.dir/src/SudokuGame.cpp.obj.d"
+  "D:/Aly/Software Course/Projects/Sudoku Project/src/SudokuSolver.cpp" "CMakeFiles/SudokuProject.dir/src/SudokuSolver.cpp.obj" "gcc" "CMakeFiles/SudokuProject.dir/src/SudokuSolver.cpp.obj.d"
+  "D:/Aly/Software Course/Projects/Sudoku Project/src/main.cpp" "CMakeFiles/SudokuProject.dir/src/main.cpp.obj" "gcc" "CMakeFiles/SudokuProject.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
